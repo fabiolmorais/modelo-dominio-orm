c771d3bf6025a472c73defd3f23ef8da2b52ab00
fabiomorais <fabio.lmorais99@gmail.com>
2024-03-24 18:01:32 -0300

    Criado a entidade Participante, feito o relacionamento muitos-para-muitos e o seeding do banco de dados

--- src/main/java/com/desafio/modelodominioorm/entities/Atividade.java
+++ src/main/java/com/desafio/modelodominioorm/entities/Atividade.java
@@ -2,9 +2,7 @@ package com.desafio.modelodominioorm.entities;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_atividade")
@@ -24,6 +22,9 @@ public class Atividade {
 
     @OneToMany(mappedBy = "atividade")
     private List<Bloco> blocos = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
 
     public Atividade(){
     }
@@ -78,6 +79,10 @@ public class Atividade {
 
     public List<Bloco> getBlocos() {
         return blocos;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
     }
 
     @Override
